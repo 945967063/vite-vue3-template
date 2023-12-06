2b4b836c56b4369365a156adea1dd1f71e1898b2
linian <945967063@qq.com>
2023-12-06 14:56:26 +0800

    update: login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <vue-particles id="tsparticles" :particlesInit="particlesInit" :options="options" />
   <div class="w-full h-full flex-c relative">
     <el-form class="w-[300px]" ref="ruleFormRef" :model="ruleForm" :rules="loginRules" status-icon>
       <el-form-item
@@ -18,7 +19,7 @@
           :placeholder="$t('login.loginAccount')"
         />
       </el-form-item>
-      <el-form-item class="mt-5" prop="password">
+      <el-form-item class="mt-5" prop="passWord">
         <el-input
           v-model.trim="ruleForm.passWord"
           type="password"
@@ -50,6 +51,8 @@
   import type { FormInstance } from 'element-plus';
   import { loginRules } from './utils/rule';
   import { login } from '@/api/login';
+  import { loadSlim } from 'tsparticles-slim';
+  import imageBg from '@/assets/images/bg.jpg';
   interface RuleForm {
     useName: string;
     passWord: string;
@@ -90,5 +93,92 @@
         console.log('error submit!', fields);
       }
     });
+  };
+
+  // <!--引入粒子特效的相关配置-->
+  const options = {
+    background: {
+      color: {
+        value: '', //粒子颜色
+      },
+      image: `url(${imageBg})`,
+      position: '50% 50%',
+      repeat: 'no-repeat',
+      size: 'cover',
+      opacity: 1,
+    },
+    fpsLimit: 60,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: 'push', //可用的click模式有: "push", "remove", "repulse", "bubble"。
+        },
+        onHover: {
+          enable: true,
+          mode: 'grab', //可用的hover模式有: "grab", "repulse", "bubble"。
+        },
+        resize: true,
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.8,
+          size: 40,
+        },
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: '#f7f035',
+      },
+      links: {
+        color: '#f7f035', //'#dedede'。线条颜色。
+        distance: 150, //线条长度
+        enable: true, //是否有线条
+        opacity: 0.1, //线条透明度。
+        width: 1, //线条宽度。
+      },
+      collisions: {
+        enable: false,
+      },
+      move: {
+        direction: 'none',
+        enable: true,
+        outMode: 'bounce',
+        random: false,
+        speed: 1, //粒子运动速度。
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80, //粒子数量。
+      },
+      opacity: {
+        value: 0.5, //粒子透明度。
+      },
+      shape: {
+        type: 'star', //可用的粒子外观类型有："circle","edge","triangle", "polygon","star"
+      },
+      size: {
+        random: true,
+        value: 2,
+      },
+    },
+    detectRetina: true,
+  };
+  const particlesInit = async (engine: any) => {
+    await loadSlim(engine);
   };
 </script>
